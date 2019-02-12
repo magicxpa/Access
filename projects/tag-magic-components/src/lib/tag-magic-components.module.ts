a32197d1782e30c511b7f41a0a4ed5252e36ed10
rinavin <rvinqokurov1@gmail.com>
2019-02-12 14:51:12 +0200

    refactoring

--- projects/tag-magic-components/src/lib/tag-magic-components.module.ts
+++ projects/tag-magic-components/src/lib/tag-magic-components.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-import {TagInputFieldValueAccessor} from './tag-input-field.ac.directive';
-import {TagComboboxMagicDirective} from './tag-combobox.magic.directive';
-import {TagComboBoxValueAccessor} from './tag-combo-field.ac.directive';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {MagicModule} from '@magic-xpa/angular';
 import {BrowserModule} from '@angular/platform-browser';
-import {TagCheckboxValueAccessor} from './tag-checkbox.cva.directive';
+import {MagicModule} from '@magic-xpa/angular';
+
+import {TagInputFieldValueAccessor} from './control-value-accessors/tag-input-field.ac.directive';
+import {TagComboboxMagicDirective} from './tag-combobox.magic.directive';
+import {TagComboBoxValueAccessor} from './control-value-accessors/tag-combo-field.ac.directive';
+
+import {TagCheckboxValueAccessor} from './control-value-accessors/tag-checkbox.cva.directive';
 import {TagCheckboxMagicDirective} from './tag-checkbox.magic.directive';
 
-const decs = [TagInputFieldValueAccessor, TagComboboxMagicDirective, TagComboBoxValueAccessor, TagCheckboxValueAccessor, TagCheckboxMagicDirective];
+const decs = [TagInputFieldValueAccessor, TagComboboxMagicDirective, TagComboBoxValueAccessor, TagCheckboxValueAccessor,
+  TagCheckboxMagicDirective];
 
 @NgModule({
   declarations: [...decs],
